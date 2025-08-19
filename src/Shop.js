de68e878e87fa9e90d255aee63cfd2c81fe07341
Vipul Solanki <vipul@logicrays.com>
2025-08-19 14:44:49 +0530

    Shop App initialized

---
+++ src/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container mt-3'>
+        <h4 className='mb-3 mt-3'>Vips Shop</h4>
+        <h6 className='mb-3 mt-3'>Buy Adidas Shoes for 50</h6>
+        <button className="btn btn-primary"> + </button>
+        <span className='pr-3'> Add To Cart </span>
+        <button className="btn btn-primary"> - </button>
+
+    </div>
+  )
+}
+
+export default Shop
